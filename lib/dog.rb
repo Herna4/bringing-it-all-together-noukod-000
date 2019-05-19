5fb15350cc1356c43e915c59ba7f995214145468
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 21:26:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,8 +10,8 @@ require_relative "../config/environment.rb"
       @breed = breed
     end
 
-     def self.create_table
-       sql = <<-SQL
+    def self.create_table
+      sql = <<-SQL
          CREATE TABLE IF NOT EXISTS dogs (
          id INTEGER PRIMARY KEY,
          name TEXT,
@@ -19,12 +19,12 @@ require_relative "../config/environment.rb"
          )
       SQL
       DB[:conn].execute(sql)
-   end
+    end
 
     def self.drop_table
       sql = "DROP TABLE IF EXISTS dogs"
       DB[:conn].execute(sql)
-   end
+    end
 
     def save
       if self.id
@@ -38,20 +38,20 @@ require_relative "../config/environment.rb"
          @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
       end
       self
-   end
+    end
 
     def self.create(name:, breed:)
       dog = self.new(name: name, breed: breed)
       dog.save
       dog
-   end
+    end
 
     def self.new_from_db(row)
       id = row[0]
       name = row[1]
       breed = row[2]
       self.new(name: name, breed: breed, id: id)
-   end
+    end
 
     def self.find_by_id(id)
       sql = <<-SQL
