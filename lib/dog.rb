42f173d66c872f8a031bc386f98e5b0f84d86d99
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 21:18:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,9 @@ class Dog
   attr_reader :id 
   
   def initialize(id=nil, name, breed)
-    @id = id 
-    @name = name 
-    @breed = breed
-  end
+    self.id = id
+    self.name = name
+    self.breed = breed
   
   def self.create_table
     sql = <<-SQL
